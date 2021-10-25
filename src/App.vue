b6ad8a51bea05b6a7c271dc82767c78a53a4aa01
Muhammed Ramees <muhammed.ramees.17@mountblue.tech>
2021-10-25 14:06:50 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header id="header" />
+
+    <div id="form">
+      <Form />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from "./components/Header.vue";
+import Form from "./components/form.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
+    Form,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif git push --set-upstream origin main
+#form {
+  width: 50%;
+  height: 50%;
+  margin: 0 auto;
 }
 </style>
