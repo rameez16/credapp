14fc1e58abddb37e45bb8d396c95ba073a4d464b
Muhammed Ramees <muhammed.ramees.17@mountblue.tech>
2021-10-25 11:53:24 +0530

    initial commit

---
+++ Vfire/src/assets/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBR_R1J7ii6ZUyRbFVYUbfS929ocjupWys",
+
+  authDomain: "fir-vue-8e086.firebaseapp.com",
+
+  projectId: "fir-vue-8e086",
+
+  storageBucket: "fir-vue-8e086.appspot.com",
+
+  messagingSenderId: "908680726985",
+
+  appId: "1:908680726985:web:2c5661b196fbc3173d0018",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
---
+++ Vfire/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './firebase'
+import VueFire from 'vuefire'
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
